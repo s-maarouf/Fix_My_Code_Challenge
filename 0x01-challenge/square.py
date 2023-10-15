1ceca4b146b6bb6b0e461cf4b8ecbd2ae8046614
s-maarouf <maarouf.salahedd@gmail.com>
2023-10-15 20:45:51 +0100

    Fixed task 1: typo fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """Perimeter of the square"""
